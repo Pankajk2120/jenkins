dfc49e7282a42594b9e00615645f9bff09e1c45b
Pankajk2120 <khadepankaj30@gmail>
2024-08-16 16:58:39 +0530

    First Commit

---
+++ src/test/java/com/mavenforjenkins1/Sample.java
@@ -0,0 +1,24 @@
+package com.mavenforjenkins1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	
+	@Test
+	public void startTesting() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		System.out.println("-----");
+		String Title = driver.getTitle();
+		System.out.println("Title of the WebPage : " +Title);
+		//driver.quit();
+	}
+}
